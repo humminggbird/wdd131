2f88d7e24f8a566c3c88b4dc0cee00eb2d115bd3
humminggbird <jdadzie@byupathway.edu>
2026-06-10 21:34:44 +0000

    siteplan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+
+const year = document.getElementById("currentyear");
+year.textContent = new Date().getFullYear();
+
+
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
